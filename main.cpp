9921ecab326ab8001f8caf1dfff20af7a8cf6e23
Cache Makers <Image15@iMac-LL.local>
2017-01-19 17:11:54 -0700

    Edited main

--- main.cpp
+++ main.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
 
+//Main code
+int main() {
+  int turn = 0;
+  bool gameOver = false;
 
-int main()
-{
   std::cout << "Welcome to Tic-Tac-Toe (Samsung13M Version)" << std::endl;
+  //Play game if there is not a win or loss
+  while(gameOver == false) {
+    //Checks if turn is player one
+    if(turn == 0) {
+      std::cout << "It is Player One's Turn!" << std::cout;
+    }
+    //Checks if turn is player two
+    else {
+      std::cout << "It is Player Two's Turn!" << std::cout;
+      }
+
+    //do the turn
+    turn++;
+    turn %= 2; //Alternates turn
+    }
 }
