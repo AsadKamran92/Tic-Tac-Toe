0839ba292a8475188bceb79b3ab1a309052eab83
Cache Makers <Image15@iMac-J.local>
2017-01-26 17:33:40 -0700

    butterified the board

--- main.cpp
+++ main.cpp
@@ -5,14 +5,23 @@ char board[3][3] = {{' ',' ',' '},{' ',' ',' '},{' ',' ',' '}};
 
 //Prints the board
 void printBoard() {
+  std::cout << " 0   1   2" << std::endl;
 
   for(int i=0; i<3; i++)
   {
+    std::cout << i << " ";
     for(int j=0; j<3; j++)
     {
       std::cout << board[i][j];
+      if(j<2){
+
+      std::cout << " | ";
     }
-    std::cout << std::endl;  }
+    }
+    std::cout << std::endl;
+    std::cout << "  _________" << std::endl;
+
+    }
 }
 
 //Main code
